62de93848bdccf51a51125757fe5797f9e98d349
Steve McDonald <steve@eastnorthwest.com>
2017-06-21 08:52:56 -0700

    Added mocha, chai unit tests itemsInSection, cheapItems, itemsInSection in test/database_test.js

--- part-2/test/database_test.js
+++ part-2/test/database_test.js
@@ -1,19 +1,42 @@
 const db = require('../database.js');
 const chai = require('chai');
+const chaiSubset = require('chai-subset');
+chai.use(chaiSubset);
+
 const assert = chai.assert;
 
-describe('allItems', () => {
-
-  it('should be at least one item in length', () => {
+describe('itemsInSection', () => {
+  it('A call to itemsInSection("bulk") returns the items "Flour", "Pasta", and "Rice"', () => {
 
     return Promise.all([
       db.itemsInSection("bulk").then(
-          function(d) {
-            assert.include(d, {name: "Flour"});
-          }).catch(
-          (reason) => {
-            console.log("ERROR:allItems", reason);
-          })]);
-    })
+          function (d) {
+            assert.containSubset(d, [{'name' : 'Flour'}]);
+            assert.containSubset(d, [{'name' : 'Pasta'}]);
+            assert.containSubset(d, [{'name' : 'Rice'}]);
+          })])
+  })
+});
 
+describe('cheapItems', () => {
+  it('A call to cheapItems() returns the item "Fish" as the first item and "Honey" as the last item', () => {
+
+    return Promise.all([
+      db.cheapItems().then(
+          function (d) {
+            assert.propertyVal(d[0], 'name', 'Fish');
+            assert.propertyVal(d[(d.length -1)], 'name', 'Honey');
+          })])
+  })
+});
+
+describe('itemsInSection', () => {
+  it('A call to countItemsInSection("packaged") returns 5', () => {
+
+    return Promise.all([
+      db.countItemsInSection("packaged").then(
+          function (d) {
+            assert.equal(d[0].count, 5);
+          })])
+  })
 });
