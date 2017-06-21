3ec6ef762a47f64b64515864dff3ce6b5fdcedbc
Steve McDonald <steve@eastnorthwest.com>
2017-06-21 11:21:15 -0700

    Completed cart functions

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -20,6 +20,11 @@ document.getElementById("cart-button").addEventListener("click", () => {
   openModal();
 })
 
+// open modal
+document.getElementById("clearButton").addEventListener("click", () => {
+  clearCart();
+})
+
 window.addEventListener("click", (el) => {
   if (el.target == document.getElementById("modalClose") ) {
     closeModal();
@@ -29,16 +34,20 @@ window.addEventListener("click", (el) => {
 function addToCart(el) {
   var name = el.target.parentElement.getElementsByClassName("item-name")[0].textContent;
   var price = el.target.parentElement.getElementsByClassName("item-price")[0].textContent;
-  cart.total = 0;
 
   cart.items.push({
     'name' : name,
     'price' : price.substr(1)
   });
+  updateCart();
+}
 
+function updateCart() {
+  cart.total = 0;
   cart.items.forEach((item) => {
-    cart.total += parseFloat(parseInt(item.price*100)/100);
+    cart.total += (parseInt(item.price*100));
   });
+  cart.total = formatAmount(cart.total/100);
   document.getElementById("cart-item-count").innerText = '(' + cart.items.length + ')';
 }
 
@@ -51,4 +60,43 @@ function openModal() {
 function closeModal() {
   console.log('close')
   document.getElementById("modal").style.display = 'none';
+}
+
+function populateCart() {
+  var html = '';
+  if (cart.items.length) {
+    cart.items.forEach((item) => {
+      html+=
+      '<div class="flex cart-line-item">' +
+      '<div class="flex-column cart-line-item-title">' + item.name + '</div>' +
+      '<div class="flex-column cart-line-item-price">' + formatAmount(item.price) + '</div>' +
+      '</div>'
+    })
+    document.getElementById('cartDisplayTotal').innerText = formatAmount(cart.total);
+    document.getElementById('total').style.display = 'flex';
+  } else {
+    html = '<p>No items in cart.</p>';
+    document.getElementById('total').style.display = 'none';
+  }
+  document.getElementById('line-items').innerHTML = html;
+}
+
+function clearCart() {
+  closeModal();
+  cart.items = [];
+  updateCart();
+}
+
+function formatAmount(amount) {
+  if (typeof amount == 'string') {
+    amount = amount.replace(/\$/g,'');
+  }
+  var showZero = '';
+  if ((amount * 10) == parseInt(amount * 10)) {
+    showZero = '0';
+  }
+  if ((amount) == parseInt(amount)) {
+    showZero = '.00';
+  }
+  return '$' + amount + showZero;
 }
